2a8000389302ffb9fa0bd480ec82161454a90d18
20181106279 <1771478860@qq.com>
2019-09-06 09:32:52 +0800

    aa
    aa

--- www/main.swift
+++ www/main.swift
@@ -1,12 +1,21 @@
-//
-//  main.swift
-//  www
-//
-//  Created by 20181106279 on 2019/9/6.
-//  Copyright © 2019 20181106279. All rights reserved.
-//
+print("请输入您的分数：")
+var score = readLine()!
+var index = Float(score)!
+if index>=90&&index<=100 {
+    print("A")
+}
+else if index>=80{
+    print("B")
+}
+else if index>=70{
+    print("C")
+}
+else if index>=60{
+    print("D")
+}
+else {
+    print("不及格")
+}
 
-import Foundation
 
-print("Hello, World!")
 
